556e8988fcb7201151350aa4ce50cc914670169d
Melissa Paisley <mpaisley@atlassian.com>
2021-06-04 11:40:24 +1000

    updating the way progress is rendered

--- public/client.js
+++ public/client.js
@@ -32,46 +32,55 @@ TrelloPowerUp.initialize({
     }];
   },
   'card-badges': function(t, options) {
-    return t.get('card','shared')
-    .then(function(data) {
-      console.log(data);
-      return [{
-        icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
-        text: data.progress + '%'
-      },{
-        icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
-        text: data.size
-      }];  
-    });
+    return t.list('name')
+        .then(function(list){
+          return t.get('card','shared')
+              .then(function(data) {
+                if(typeof data.progress === 'undefined'){
+                  switch(list.name) {
+                    case "Complete":
+                      t.set('card','shared','progress', 100);
+                      break
+                    default:
+                      t.set('card','shared','progress', 0);
+                  }
+                }
+                if(typeof data.size === 'undefined'){
+                  t.set('card','shared','size', "NA")
+                }
+                return [{
+                  icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
+                  text: data.progress + '%'
+                },{
+                  icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
+                  text: data.size
+                }];
+              });
+        });
   },
   'card-detail-badges': function (t, opts) {
     return t.get('card','shared')
-      .then(function (cardData) {
-
-        return [
-          {
-            // its best to use static badges unless you need your badges
-            // to refresh you can mix and match between static and dynamic
+        .then(function (cardData) {
+          var cardBadge = [{
             title: "Size",
             text: cardData.size,
-            color: null,
-          },
-          {
-            // or for simpler use cases you can also provide a url
-            // when the user clicks on the card detail badge they will
-            // go to a new tab at that url
-            title: "Pattern",
-            text: cardData.patternName,
-            url: cardData.patternLink,
-            target: "Link to Pattern", // optional target for above url
-          }, {
-            // its best to use static badges unless you need your badges
-            // to refresh you can mix and match between static and dynamic
-            title: "Yarn Details",
-            text: cardData.yarn + " " + cardData.weight + " in " + cardData.colour,
-            color: null,
-          },
-        ];
-      });
+            color: null,}];
+          var regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+
+          if(regexp.test(cardData.patternLink)){
+            cardBadge.push({
+              title: "Pattern",
+              text: cardData.patternName,
+              url: cardData.patternLink,
+              target: "Link to Pattern", // optional target for above url
+            })
+          } else {
+            cardBadge.push({
+              title: "Pattern",
+              text: cardData.patternName,
+            })
+          };
+          return cardBadge;
+        });
   },
 });
